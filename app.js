f089581f0acf96a805185bfbd202d22b61c4db8c
TamNguyenThien <nguyentant119696@gmail.com>
2019-07-01 10:21:12 +0700

    first cmt

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+const path = require('path')
+const Joi = require('joi')
+
+const db = require('./db')
+const collection = 'todo'
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+// read
+app.get('/getTodos', (req, res) => {
+  db.getDB().collection(collection).find({}).toArray((err, documents) => {
+    if (err) { console.log(err) } else {
+      console.log(documents)
+      res.json(documents)
+    }
+  })
+})
+// update
+app.put('/:id', (req, res) => {
+  const todoID = req.params.id
+  const userInput = req.body
+  db.getDB().collection(collection).findOneAndUpdate({ _id: db.getPrimaryKey(todoID) }, { $set: { todo: userInput.todo } }, { returnOriginal: false }, (err, result) => {
+    if (err) { console.log(err) } else { res.json(result) }
+  })
+})
+// create
+app.post('/', (req, res) => {
+  const userInput = req.body
+  db.getDB().collection(collection).insertOne(userInput, (err, result) => {
+    if (err) { console.log(err) } else { res.json({ result: result, document: result.ops[0] }) }
+  })
+})
+// delete
+app.delete('/:id', (req, res) => {
+  const todoID = req.params.id
+  db.getDB().collection(collection).findOneAndDelete({ _id: db.getPrimaryKey(todoID) }, (err, result) => {
+    if (err) { console.log(err) } else { res.json(result) }
+  })
+})
+
+db.connect((err) => {
+  if (err) {
+    console.log('unable to connect to db')
+    process.exit(1)
+  } else {
+    app.listen(3000, () => {
+      console.log('connected to db,app listening on port 3000')
+    })
+  }
+})
